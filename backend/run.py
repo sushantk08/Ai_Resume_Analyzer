c3ae6dd621da9327e49515303e7cf4613cb24cb3
Ashvini9657 <khareashvini2002@gmail.com>
2026-07-30 15:05:51 +0530

    debugging

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,11 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=True
+    )
