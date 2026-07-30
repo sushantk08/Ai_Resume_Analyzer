64c91458a824a3cd3d10683323b8bbc07a793394
Ashvini9657 <khareashvini2002@gmail.com>
2026-07-30 12:55:22 +0530

    Prepare project for deployement

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,12 +7,15 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "secret-key")
 
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{os.getenv('DB_USER')}:"
-        f"{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}:"
-        f"{os.getenv('DB_PORT')}/"
-        f"{os.getenv('DB_NAME')}"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        (
+            f"postgresql://{os.getenv('DB_USER')}:"
+            f"{os.getenv('DB_PASSWORD')}@"
+            f"{os.getenv('DB_HOST')}:"
+            f"{os.getenv('DB_PORT')}/"
+            f"{os.getenv('DB_NAME')}"
+        ),
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
