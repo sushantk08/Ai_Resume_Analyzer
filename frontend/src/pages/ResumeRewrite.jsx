861879b2a5d2d60210e605eacc2009803956d755
Ashvini9657 <khareashvini2002@gmail.com>
2026-07-30 16:32:13 +0530

    Add Resumewirte placeholder

---
+++ frontend/src/pages/ResumeRewrite.jsx
@@ -0,0 +1,10 @@
+function ResumeRewrite() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Resume Rewrite</h1>
+      <p>This feature is coming soon.</p>
+    </div>
+  );
+}
+
+export default ResumeRewrite;
